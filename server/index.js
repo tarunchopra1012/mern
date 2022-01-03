863d1d3a248f5971aecfed0138f377984693ec3e
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-03 08:34:11 +0530

    Changing the database name

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb://127.0.0.1:27017/javascriptmastery';
+const CONNECTION_URL = 'mongodb://127.0.0.1:27017/memories';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
