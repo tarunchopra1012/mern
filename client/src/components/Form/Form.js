fd78e0890eb58c5386bdd59048feca2f9bde80f5
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-06 08:10:09 +0530

    Form Layout & Send post data to api through reducers

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -9,9 +11,12 @@ const Form = () => {
     });
 
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
@@ -20,7 +25,7 @@ const Form = () => {
     
     return (
         <Paper className={classes.paper}>
-            <form autoComplete='off' noValidate className='{classes.form}' onSubmit={handleSubmit}>
+            <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant='h6'>Creating a Memory</Typography>
                 <TextField 
                     name='creator' 
