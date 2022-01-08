64d61efc8e43ae5e22f933bff8cb4ad712ca77b2
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-08 21:17:11 +0530

    Edit Post, clear form & Reflect changes to original post.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
     try {
@@ -31,7 +32,7 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatedPost);
 }
