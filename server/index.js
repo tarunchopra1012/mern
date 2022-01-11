903facfaef34863e50d7b34f93135d6fb653b582
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-11 22:24:26 +0530

    Make public route.

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to the memories API');
+});
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
